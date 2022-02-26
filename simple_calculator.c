ef128c2bc8db6d24121f8924bd260a93ce99fdb9
kay-san <kay.sanjit@gmail.com>
2022-02-26 14:54:47 +0530

    Modified Addition

--- simple_calculator.c
+++ simple_calculator.c
@@ -1,16 +1,33 @@
 #include<stdio.h>
+void Addition (float, float);
 int main(){
     int i;
-printf("Enter your choice");
-scanf("%d",&i);
-switch(i){
-    case 1:
-    case 2:
-    case 3:
-    case 4:
-    
+    float num1, num2, sum;
+    printf("Enter 1st number : ");
+    scanf("%f", &num1);
+    printf("Enter 2nd number : ");
+    scanf("%f", &num2);
 
-}
+    printf("\nEnter your choice");
+    printf("\n1: Additon\n2: Subtraction\n3: Multiplication\n4: Division\n");
+    
+    scanf("%d",&i);
+    switch(i){
+        case 1: Addition(num1, num2);
+        break;
+        case 2:
+        break;
+        case 3:
+        break;
+        case 4:
+        break;
+        default:
+        break;
+    }
 
 return 0;
+}
+
+void Addition(float num1, float num2){
+    printf("Addition = %.2f", num1 + num2);
 }
